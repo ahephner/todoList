2cd39d1939643e0520b372e22d83552be1da5c86
AJ Hephner <ajhephner178@gmail.com>
2017-12-05 15:23:33 -0500

    added update

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -6,14 +6,20 @@ $(document).ready(function(){
 
     $('#todoInput').keypress(function(event){
         if(event.which == 13){
+            $('#search').click();
             createTodo();
         }
     })    
 
+    $('.list').on('click', 'li', function(){
+      updateTodo($(this));
+    })
     //due to that the list class is on the page a load time we target
     //it for the click pass 'span' as what individual we are targeting on the class
     //common issue with single page due to when the span is added after page loaded
-    $('.list').on('click', 'span', function(){
+    $('.list').on('click', 'span', function(event){
+    //this will stop the the li function from being triggered 
+        event.stopPropagation();
        removeTodo($(this).parent());
     })
 
@@ -31,6 +37,7 @@ function addTodo(todo){
     var newTodo = $('<li class="task">' + todo.name  + '<span>X</span></li>')
     //this allows us to assign new data when the new li is made so we can reference it else where
     newTodo.data('id', todo._id );
+    newTodo.data('completed', todo.completed)
     if(todo.completed){
         newTodo.addClass('done');
     }
@@ -62,4 +69,20 @@ function removeTodo(todo){
     .then(function(data){
         todo.remove();
     });
+}
+
+function updateTodo(todo){
+    
+    var updateURL = 'api/todos/' + todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone}
+$.ajax({
+    method: 'PUT',
+    url: updateURL,
+    data: updateData
+})
+.then(function(updatedTodo){
+    todo.toggleClass('done');
+    todo.data('completed', isDone);
+})
 }
