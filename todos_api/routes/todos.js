6bacf755a63dae5c50cc5d9f93d615f3de484b9c
AJ Hephner <ajhephner178@gmail.com>
2017-12-05 10:12:57 -0500

    added update delete routes

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -27,4 +27,37 @@ router.post('/', function(req, res){
     })
 });
 
+
+//get older entered items by id created from mongo
+router.get('/:todoId', function(req, res){
+    //findById is to get the id of the item
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+});
+
+router.put('/:todoId', function(req, res){
+    //db Todo findOneAndUpdate will update item req.body is what will be the new data and {new: true} gives me the response
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+    .then(function(todo){
+        res.json(todo)
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+})
+
+router.delete('/:todoId', function(req, res){
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: "Item Deleted"});
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+})
 module.exports = router; 
