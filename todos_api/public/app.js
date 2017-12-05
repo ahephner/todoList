83c106e6ced1e31751bbe09fb6833ff9a80ca322
AJ Hephner <ajhephner178@gmail.com>
2017-12-05 14:07:14 -0500

    added ability to add a new list item

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+$(document).ready(function(){
+    $.getJSON('http://localhost:3000/api/todos')
+        .then(addTodos)
+
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    })    
+
+
+});
+   
+function addTodos(todos){
+    //add here
+    todos.forEach(function(todo){
+     addTodo(todo);
+    })
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="task">' + todo.name  + '</li>')
+    if(todo.completed){
+        newTodo.addClass('done');
+    }
+    $('.list').append(newTodo)
+}
+
+
+function createTodo(){
+//send new task
+var userInput = $('#todoInput').val();
+$.post('http://localhost:3000/api/todos', {name: userInput})
+    .then(function(newTodo){
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
